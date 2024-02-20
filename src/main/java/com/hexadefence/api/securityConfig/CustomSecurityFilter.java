4a1e796df9ffbac9e7f2bb1f93eb571df2ad4228
hexadefence <hexadefence@gmail.com>
2024-02-20 09:16:07 +0530

    code refactoring

--- src/main/java/com/hexadefence/api/securityConfig/CustomSecurityFilter.java
+++ src/main/java/com/hexadefence/api/securityConfig/CustomSecurityFilter.java
@@ -17,13 +17,20 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.security.interfaces.RSAPublicKey;
-import java.util.Arrays;
+import java.util.List;
 
 public class CustomSecurityFilter implements Filter {
 
     Logger logger = LoggerFactory.getLogger(getClass());
+
+    JwkProvider jwkProvider;
+
+    public CustomSecurityFilter() throws MalformedURLException {
+        jwkProvider = new JwkProviderBuilder(new URL("http://localhost:8080/realms/master/protocol/openid-connect/certs")).build();
+    }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
@@ -33,13 +40,14 @@ public class CustomSecurityFilter implements Filter {
             String authorizationHeader = ((HttpServletRequest) request).getHeader("Authorization");
             String token = authorizationHeader.substring(7);
 
-            JwkProvider jwkProvider = new JwkProviderBuilder(new URL("http://localhost:8080/realms/master/protocol/openid-connect/certs")).build();
-
             DecodedJWT decodedJWT = JWT.decode(token);
+
+            // start verification process
             Jwk jwk = jwkProvider.get(decodedJWT.getKeyId());
 
-
+            // Assuming all tokens are signed using RSA256 algorithm otherwise algorithm can be found using jwk.getAlgorithm() method
             Algorithm algorithm = Algorithm.RSA256((RSAPublicKey) jwk.getPublicKey(), null);
+
             JWTVerifier verifier = JWT.require(algorithm)
                     .withIssuer("http://localhost:8080/realms/master")
                     .withAudience("backend-api")
@@ -48,12 +56,12 @@ public class CustomSecurityFilter implements Filter {
 
             SecurityContextHolder.getContext().setAuthentication(
                     new UsernamePasswordAuthenticationToken(decodedJWT.getSubject(), "***",
-                            Arrays.asList(new SimpleGrantedAuthority("SIMPLE_AUTHORITY"))));
+                            List.of(new SimpleGrantedAuthority("SIMPLE_AUTHORITY"))));
 
 
 
         } catch (JWTVerificationException jwtVerificationException){
-            logger.error("Verification Excepton", jwtVerificationException);
+            logger.error("Verification Exception", jwtVerificationException);
         }
         catch (Exception e){
             logger.error("Exception", e);
